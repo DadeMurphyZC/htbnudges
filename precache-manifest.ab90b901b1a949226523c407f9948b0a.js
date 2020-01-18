688cff4745729b2c2eec071729b0bbce6cbf3e77
Cristoval Fonseca <cfonseca@protonmail.com>
2020-01-18 11:49:51 -0800

    Updates

---
+++ precache-manifest.ab90b901b1a949226523c407f9948b0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a318c2c4c8091dc3d31be4cd871dc25",
+    "url": "/htbnudges/index.html"
+  },
+  {
+    "revision": "91ce5045f112d500db29",
+    "url": "/htbnudges/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "793d7b2fb3707169ea47",
+    "url": "/htbnudges/static/js/2.e6786523.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/htbnudges/static/js/2.e6786523.chunk.js.LICENSE"
+  },
+  {
+    "revision": "91ce5045f112d500db29",
+    "url": "/htbnudges/static/js/main.1d860b91.chunk.js"
+  },
+  {
+    "revision": "434a4d39cea074860370",
+    "url": "/htbnudges/static/js/runtime-main.5eeae6c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/htbnudges/static/media/logo.5d5d9eef.svg"
+  }
+]);
